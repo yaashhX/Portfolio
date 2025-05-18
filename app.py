2b8054ea2cbd3aa79dab5f83ebc2feb9a50a4ff1
yaashhX <yk5505564@gmail.com>
2025-05-18 13:45:06 +0530

    Add HTTPS redirect to force secure connection

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+
+@app.before_request
+def redirect_to_https():
+    if request.headers.get('X-Forwarded-Proto', 'http') == 'http':
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
